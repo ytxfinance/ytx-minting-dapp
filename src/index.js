a8288c584b22fbca8f052a5a75c7f5f0bb5dbcff
Andrew <ampatte2@asu.edu>
2020-12-04 16:41:00 -0700

    changed to js files for webpack

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css'
-import { Home } from './pages/Home'
+import { Home } from './pages'
 import { CardStoreProvider } from './store'
 
 function App () {
